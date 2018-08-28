7b09eb2b44ab82b4ac0971b50907cc6ad42cd043
Minamiciccc <june28munazo@gmail.com>
2018-08-27 18:23:14 -0700

    create dialog and holder

---
+++ app/src/main/java/com/minami/project/android/memorizingnumbersapp/DataBaseHelper.java
@@ -0,0 +1,10 @@
+package com.minami.project.android.memorizingnumbersapp;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Minami on 2018/08/24.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+}
