66fe24e52d7c81179f9292a3a2e5ce5999175bb5
Minamiciccc <june28munazo@gmail.com>
2018-08-29 16:37:22 -0700

    create a viewholder

--- app/src/main/java/com/minami/project/android/memorizingnumbersapp/MyRecyclerViewHolder.java
+++ app/src/main/java/com/minami/project/android/memorizingnumbersapp/MyRecyclerViewHolder.java
@@ -5,15 +5,22 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by Minami on 2018/08/24.
  */
 
 public class MyRecyclerViewHolder extends RecyclerView.ViewHolder {
-    private LinearLayout holder;
+    private TextView category_holder;
+    private TextView item_holder;
+    private TextView org_holder;
+    private TextView code_holder;
     public MyRecyclerViewHolder(View itemView) {
         super(itemView);
-        holder = itemView.findViewById(R.id.holder);
-        
+        category_holder = itemView.findViewById(R.id.category_holder);
+        item_holder = itemView.findViewById(R.id.item_holder);
+        org_holder = itemView.findViewById(R.id.org_holder);
+        code_holder = itemView.findViewById(R.id.code_holder);
     }
 }
