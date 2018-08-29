3b2040222f65450cb2a65c60e11f0a57d981809a
Minamiciccc <june28munazo@gmail.com>
2018-08-29 15:36:21 -0700

    set menu

--- app/src/main/java/com/minami/project/android/memorizingnumbersapp/MainActivity.java
+++ app/src/main/java/com/minami/project/android/memorizingnumbersapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.minami.project.android.memorizingnumbersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.swich_activity:
+                Log.i("MENU", "onOptionsItemSelected: ---");
+                // User chose the "Settings" item, show the app settings UI...
+                item.setIcon(R.drawable.list);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
